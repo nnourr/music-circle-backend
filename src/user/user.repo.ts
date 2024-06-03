9167e9ffe4b6bd6d80cb95cd80fcdcafadefbec8
nnourr <nourqwerty123@gmail.com>
2024-06-03 00:17:43 -0400

    add user to circle and get user

--- src/user/user.repo.ts
+++ src/user/user.repo.ts
@@ -1,4 +1,4 @@
-import { arrayUnion, doc, setDoc } from "firebase/firestore";
+import { arrayUnion, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { userCollection } from "../firebase/firebase.init.js";
 import { UserInterface } from "./user.interface.js";
 
@@ -13,6 +13,30 @@ export class UserRepo {
         },
         { merge: true }
       );
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async addUserToCircle(email: string, circleCode: string) {
+    try {
+      await updateDoc(doc(userCollection, email), {
+        circles: arrayUnion(circleCode),
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getUser(email: string) {
+    try {
+      const userDocument = await getDoc(doc(userCollection, email));
+      if (!userDocument.exists || userDocument.data() === undefined) {
+        throw { error: "user not found" };
+      }
+      const user: UserInterface = userDocument.data() as UserInterface;
+
+      return user;
     } catch (error) {
       throw error;
     }
